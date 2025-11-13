9045de7a33ae6eee12e4f0e16a7a3fabe7c54cff
Riansyah96 <arkalbojong@gmail.com>
2025-11-13 13:58:04 +0700

    Initial commit

---
+++ src/main/java/com/rianscorp/caffe/system/CaffeSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rianscorp.caffe.system;
+
+/**
+ *
+ * @author apriansyah
+ */
+public class CaffeSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
